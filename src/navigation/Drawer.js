6dcf4fad6d7b8a90edfdb32e71515266e906b5df
valencaceres <vcaceres102@gmail.com>
2023-02-01 17:28:49 -0300

    cambio de rama para fixear la navegacion

--- src/navigation/Drawer.js
+++ src/navigation/Drawer.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Home from '../screens/Home'
@@ -8,11 +8,18 @@ const Drawer = createDrawerNavigator()
 
 const DrawerNavigation = () => {
   return (
-    <Drawer.Navigator initialRouteName="Home">
+    <Drawer.Navigator initialRouteName="Home" style={styles.color}>
     <Drawer.Screen name="Home" component={Home} />
     <Drawer.Screen name="Sign In" component={SignIn} />
   </Drawer.Navigator>
   )
 }
+
+const styles = StyleSheet.create({
+  color:{
+    color: 'blue',
+    backgroundColor: 'blue'
+  }
+})
 
 export default DrawerNavigation
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, ImageBackground } from 'react-native'
 import React from 'react'
 
 const Home = () => {
   return (
     <View>
+      <ImageBackground source={require('../../assets/bkg.png')} resizeMode="cover" style={styles.image}/>
       <Text>Home</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  image: {
+    flex:1,
+    justifyContent: 'center',
+    height: 800,
+    width: 500,
+  },
+})
 
 export default Home
